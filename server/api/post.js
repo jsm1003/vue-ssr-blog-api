6fa619d43a7056e1fca12789edcdde99f1f45439
JSON <838432498@qq.com>
2017-02-10 10:22:08 +0800

    修改部分格式

--- server/api/post.js
+++ server/api/post.js
@@ -2,17 +2,17 @@ const AV = require('leanengine');
 const marked = require('marked');
 const hljs = require('highlight.js')
 marked.setOptions({
-  highlight (code, lang) {
-    var res;
-    if (lang) {
-        //设置不了央视应该是这里的问题
-      res = hljs.highlight(lang, code, true).value;
-    } else {
-      res = hljs.highlightAuto(code).value;
-    }
-    return res;
-  },
-  breaks: true //这一行干什么用的
+    highlight(code, lang) {
+        var res;
+        if (lang) {
+            //设置不了央视应该是这里的问题
+            res = hljs.highlight(lang, code, true).value;
+        } else {
+            res = hljs.highlightAuto(code).value;
+        }
+        return res;
+    },
+    breaks: true //这一行干什么用的
 })
 
 //问题1： 要不要在修改文章的时候在判断一下文章是否属于用户本人呢？多一层保险？
@@ -22,8 +22,8 @@ marked.setOptions({
 //登陆用户修改其他用户的文章这里要加一个判断，以后在做
 
 module.exports = {
-    async getTopics ({query:{limit, page, id}}, res) {
-         var aQuery = new AV.Query('Article')
+    async getTopics({query: {limit, page, id}}, res) {
+        var aQuery = new AV.Query('Article')
         if (id) {
             var tag = AV.Object.createWithoutData('Tag', id)
             aQuery.equalTo('tagChosen', tag)
@@ -33,35 +33,35 @@ module.exports = {
         aQuery.skip((page - 1) * limit)
         try {
             var topics = await Promise.all([
-            aQuery.count(),
-            aQuery.find()
-        ])
-        res.json({topics, code: 200, msg: '文章列表获取成功'})
+                aQuery.count(),
+                aQuery.find()
+            ])
+            res.json({ topics, code: 200, msg: '文章列表获取成功' })
         } catch (err) {
             if (err.code === 101) {
-                res.json({code: 404, msg: '不存在该标签'})
+                res.json({ code: 404, msg: '不存在该标签' })
                 //res.render('404.html')
             }
         }
     },
-    async getArticle ({query:{id}}, res) {
+    async getArticle({query: {id}}, res) {
         var query = new AV.Query('Article')
-        try{
+        try {
             var article = await query.get(id)
-         res.json({article, code: 200, msg: '文章获取成功'})
+            res.json({ article, code: 200, msg: '文章获取成功' })
         } catch (err) {
             if (err.code === 101)
-            res.json({code: 404, msg: '文章未找到'})
+                res.json({ code: 404, msg: '文章未找到' })
             //res.render('404.html')//后面要改成这个
             else {
                 //不知道该写什么了
-               // throw new Error(err) ?
+                // throw new Error(err) ?
             }
         }
-        
+
         //太多
     },
-    async newDraft ({body:{title, content},currentUser:{id}, allTags}, res) {
+    async newDraft({body: {title, content}, currentUser: {id}, allTags}, res) {
         var user = AV.Object.createWithoutData('_User', id)
         var draft = new AV.Object('Draft')
         draft.set(title, title)
@@ -70,10 +70,10 @@ module.exports = {
         var relation = user.relation('draftChosen')
         relation.add(draft)
         await user.save()
-        res.json({allTags, newDraft, code: 200, msg:'草稿创建成功'})
+        res.json({ allTags, newDraft, code: 200, msg: '草稿创建成功' })
         //太多
     },
-    async getOldDraft ({query: {id},currentUser, allTags}, res) {
+    async getOldDraft({query: {id}, currentUser, allTags}, res) {
         var user = AV.Object.createWithoutData('_User', currentUser.id)
         var urelation = user.relation('articleChosen')
         var uQuery = urelation.query()
@@ -90,65 +90,65 @@ module.exports = {
             var tags = await tQuery.find()
             res.json({ allTags, draft, tags, code: 200, msg: '获取草稿成功' })//讲道理他应该支持...语法啊
         } else {
-            res.json({code: 404, msg:'未找到这篇文章'})//临时办法
+            res.json({ code: 404, msg: '未找到这篇文章' })//临时办法
             //res.redirect('/aaa/articles')//这条语句需要同源才可以，再想想别的办法？
             //res.render('404.html')//这种办法？//也是需要同源，这中办法应该是最佳的，
             //应该是服务器端给他一个404页面比较好
-           // res.send('HTTP STATUS: 404')
+            // res.send('HTTP STATUS: 404')
         }
         //改
-        
+
     },
-    async modifyContent ({body: {content, id}}, res) {
+    async modifyContent({body: {content, id}}, res) {
         var draft = AV.Object.createWithoutData('Draft', id)
         draft.set('content', content)
         await draft.save()
         res.json({ msg: '内容更新成功', code: 200 })
     },
-    async modifyTitle ({body: {title, id}}, res) {
+    async modifyTitle({body: {title, id}}, res) {
         var draft = AV.Object.createWithoutData('Draft', id)
         draft.set('title', title)
         await draft.save()
         res.json({ msg: '标题更新成功', code: 200 })
     },
-    async getTags (req, res, next) {
+    async getTags(req, res, next) {
         var query = new AV.Query('Tag')
         query.ascending('createdAt')
         var allTags = await query.find()
-        if(req.path === '/tags') {
-            allTags.map(i => {return i['name']})//这里就是测试一下
-            res.json({allTags, code:200, msg: '获取所有标签成功'})
+        if (req.path === '/tags') {
+            allTags.map(i => { return i['name'] })//这里就是测试一下
+            res.json({ allTags, code: 200, msg: '获取所有标签成功' })
         } else {
             req.allTags = allTags
             next()
         }
         //太多
     },
-    async addOldTag ({body: {tag: {name, objectId}, id}}, res) {
-        var tag = AV.Object.createWithoutData('Tag', objectId )
-        var draft = AV.Object.createWithoutData('Draft', id )
+    async addOldTag({body: {tag: {name, objectId}, id}}, res) {
+        var tag = AV.Object.createWithoutData('Tag', objectId)
+        var draft = AV.Object.createWithoutData('Draft', id)
         var relation = draft.relation('tagChosen')
         relation.add(tag)
         await draft.save()
         //return res.id
         res.json({ code: 200, msg: '旧标签添加成功' })
     },
-    async addNewTag ({body:{tag, id}},res) {
-        var draft = AV.Object.createWithoutData('Draft', id )
+    async addNewTag({body: {tag, id}}, res) {
+        var draft = AV.Object.createWithoutData('Draft', id)
         var nTag = new AV.Object('Tag')
         nTag.set('name', tag)
         var newTag = await nTag.save()
         var relation = draft.relation('tagChosen')
         relation.add(nTag)
-        await draft.save()      
-        res.json({newTag, code: 200, msg:'新标签添加成功'})
+        await draft.save()
+        res.json({ newTag, code: 200, msg: '新标签添加成功' })
     },
-    async removeTag ({body: {tag: {objectId}, id}},res) {
+    async removeTag({body: {tag: {objectId}, id}}, res) {
         var remove = false
         var tQuery = new AV.Query('Tag')
         var tag = await tQuery.get(objectId)
 
-        var draft = AV.Object.createWithoutData('Draft', id )
+        var draft = AV.Object.createWithoutData('Draft', id)
         var relation = draft.relation('tagChosen')
         relation.remove(tag)
         await draft.save()
@@ -156,15 +156,15 @@ module.exports = {
         var dQuery = new AV.Query('Draft')
         dQuery.equalTo('tagChosen', tag)
         var resDraft = await dQuery.find()
-        if(! resDraft.length) {
+        if (!resDraft.length) {
             await tag.destroy()
             remove = true
         }
-        res.json({msg:'标签删除成功',code: 200, remove})
+        res.json({ msg: '标签删除成功', code: 200, remove })
 
     },
     //用户未发布草稿列表
-    async getDrafts ({query: {page, limit},currentUser:{id}}, res) {
+    async getDrafts({query: {page, limit}, currentUser: {id}}, res) {
         var user = AV.Object.createWithoutData('_User', id)
         var uRelation = user.relation('draftChosen')
         var dQuery = uRelation.query()
@@ -175,72 +175,72 @@ module.exports = {
         var drafts = await Promise.all([
             dQuery.count(),
             dQuery.find()
-        ])     
-        res.json({drafts, code: 200, msg:'草稿列表获取成功'})
+        ])
+        res.json({ drafts, code: 200, msg: '草稿列表获取成功' })
     },
-    async getNewDraft ({query:{id},currentUser, allTags}, res) {
+    async getNewDraft({query: {id}, currentUser, allTags}, res) {
         var user = AV.Object.createWithoutData('_User', currentUser.id)
         var dRelation = user.relation('draftChosen')
         var dQuery = dRelation.query()
         dQuery.equalTo('objectId', id)
-        var find = await dQuery.first()      
-        if(find) {
+        var find = await dQuery.first()
+        if (find) {
             var query = new AV.Query('Draft')
             var draft = await query.get(id)
             var relation = draft.relation('tagChosen')
             var tQuery = relation.query()
             tQuery.ascending('createdAt')
             var tags = await tQuery.find()
-            res.json({allTags, draft, tags, code:200, msg:'草稿获取成功'})
+            res.json({ allTags, draft, tags, code: 200, msg: '草稿获取成功' })
         } else {
-            res.json({code: 404, msg:'未找到这篇文章'})//临时办法
+            res.json({ code: 404, msg: '未找到这篇文章' })//临时办法
             //res.render('404.html')//这种办法？//也是需要同源，这中办法应该是最佳的，
             //应该是服务器端给他一个404页面比较好
         }
-        
+
     },
     //发布新文章
-     async publishNew ({body:{draft,tags,user}}, res) {
-         var oDraft = AV.Object.createWithoutData('Draft', draft.objectId ) 
-         var User = AV.Object.createWithoutData('_User', user.objectId)
-         var article = new AV.Object('Article')
-         var tRelation = article.relation('tagChosen')
-         var aRelation = User.relation('articleChosen')
-         tags.forEach(i => {
-             tRelation.add(AV.Object.createWithoutData('Tag', i.objectId ))
-         })
-         article.set('authname', user.username)
-         article.set('authId', user.objectId)
-         //把user的信息设置成对象的格式应该也可以，暂时先这样
-         article.set('title', draft.title)
-         article.set('content', marked(draft.content))
-         await article.save()
-         oDraft.set('dependent', article)
-         await oDraft.save()
-         aRelation.add(article)
-         await User.save()
+    async publishNew({body: {draft, tags, user}}, res) {
+        var oDraft = AV.Object.createWithoutData('Draft', draft.objectId)
+        var User = AV.Object.createWithoutData('_User', user.objectId)
+        var article = new AV.Object('Article')
+        var tRelation = article.relation('tagChosen')
+        var aRelation = User.relation('articleChosen')
+        tags.forEach(i => {
+            tRelation.add(AV.Object.createWithoutData('Tag', i.objectId))
+        })
+        article.set('authname', user.username)
+        article.set('authId', user.objectId)
+        //把user的信息设置成对象的格式应该也可以，暂时先这样
+        article.set('title', draft.title)
+        article.set('content', marked(draft.content))
+        await article.save()
+        oDraft.set('dependent', article)
+        await oDraft.save()
+        aRelation.add(article)
+        await User.save()
 
-         res.json({msg: '发布成功',code: 200})
-     },
-     async publishOld ({body:{draft, tags, id}}, res) {
+        res.json({ msg: '发布成功', code: 200 })
+    },
+    async publishOld({body: {draft, tags, id}}, res) {
         var article = AV.Object.createWithoutData('Article', id)
         var relation = article.relation('tagChosen')
         var query = relation.query()
         var list = await query.find()
         list.forEach(i => {
-            relation.remove(AV.Object.createWithoutData('Tag', i.id ))
+            relation.remove(AV.Object.createWithoutData('Tag', i.id))
         })
         await article.save()//这里得保存两边，感觉好恶心啊
         //现在弄成了吧之前关联的的tags全部都删掉，然后关联新的tags
         //先暂时这样
         tags.forEach(i => {
-            relation.add(AV.Object.createWithoutData('Tag', i.objectId ))
+            relation.add(AV.Object.createWithoutData('Tag', i.objectId))
         })
         article.set('title', draft.title)
         article.set('content', marked(draft.content))
         await article.save()
-         res.json({msg: '文章修改成功',code: 200})
-     }
-    
+        res.json({ msg: '文章修改成功', code: 200 })
+    }
+
 
 }
--- server/api/user.js
+++ server/api/user.js
@@ -1,60 +1,60 @@
 const AV = require('leanengine');
 
 module.exports = {
-    async signup (req, res) {
-      var user = new AV.User()
-      user.setUsername(req.body.username)
-      user.setPassword(req.body.password)
-      user.setEmail(req.body.email)
-      var nUser = await user.signUp()
-      res.json({code: 200, msg:'用户注册成功'})
-    },
-     async login (req, res) {
-       var json = {}
-       try {
-         var user = await AV.User.logIn(req.body.username, req.body.password)
-          res.saveCurrentUser(user)
-         // res.send(user.id)
-          json = {
-            code: 200,
-            msg: '登陆成功',
-            data: user.id
-          }
-       } catch (err) {
-         if(err.code === -1){
-           json = {
-             code: -1,
-             msg: '服务器错误'
-           }
-         } else if (err.code === 210){
-           console.log(err)
-          json = {
-            code: -210,
-            msg: '用户名或密码错误'
-          }
-         }      
-       }
-       res.json(json)
-       
-    },
-    logout (req, res) {
-      req.currentUser.logOut()
-      res.clearCurrentUser() // 从 Cookie 中删除用户他只是把avos:sess字段设置为空
-      //其实下面这两句不加也可以，我就是试一下效果
-      //试了一下没有效果，好吧，不用了
-      // res.clearCookie('avos:sess')
-      // res.clearCookie('avos:sess.sig')
-      //res.redirect('/profile');
-      res.json({
-        msg: '退出成功',
-        code: 200
-      })
-    },
-     async info ({currentUser: {id}}, res) {
-       var User = AV.Object.createWithoutData('_User', id)
-       var user = await User.fetch()
-       res.json({user, code: 200, msg: '获取用户信息成功'})
+  async signup(req, res) {
+    var user = new AV.User()
+    user.setUsername(req.body.username)
+    user.setPassword(req.body.password)
+    user.setEmail(req.body.email)
+    var nUser = await user.signUp()
+    res.json({ code: 200, msg: '用户注册成功' })
+  },
+  async login(req, res) {
+    var json = {}
+    try {
+      var user = await AV.User.logIn(req.body.username, req.body.password)
+      res.saveCurrentUser(user)
+      // res.send(user.id)
+      json = {
+        code: 200,
+        msg: '登陆成功',
+        data: user.id
+      }
+    } catch (err) {
+      if (err.code === -1) {
+        json = {
+          code: -1,
+          msg: '服务器错误'
+        }
+      } else if (err.code === 210) {
+        console.log(err)
+        json = {
+          code: -210,
+          msg: '用户名或密码错误'
+        }
+      }
     }
+    res.json(json)
+
+  },
+  logout(req, res) {
+    req.currentUser.logOut()
+    res.clearCurrentUser() // 从 Cookie 中删除用户他只是把avos:sess字段设置为空
+    //其实下面这两句不加也可以，我就是试一下效果
+    //试了一下没有效果，好吧，不用了
+    // res.clearCookie('avos:sess')
+    // res.clearCookie('avos:sess.sig')
+    //res.redirect('/profile');
+    res.json({
+      msg: '退出成功',
+      code: 200
+    })
+  },
+  async info({currentUser: {id}}, res) {
+    var User = AV.Object.createWithoutData('_User', id)
+    var user = await User.fetch()
+    res.json({ user, code: 200, msg: '获取用户信息成功' })
+  }
 }
 
 //code: 206 没有sessionId
